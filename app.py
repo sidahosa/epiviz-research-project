f48373fd912d5c1717b601f8203d9c0edff132fb
Shawn Idahosa <sidahosa@users.noreply.github.com>
2017-07-23 17:42:14 -0400

    Added the get_rows function

--- app.py
+++ app.py
@@ -34,8 +34,26 @@ def search(keyword, number):
 	try:
 		x.execute("SELECT chr, start, end FROM human_genome WHERE gene LIKE %s LIMIT %d " %(keyword, number))
 		data = x.fetchall()
-		# for row in data:
-		# 	genomeList.append(row)
+		truedata = json.dumps(data)
+		return truedata
+	except:
+		return 'No Keyword found'
+
+	conn.close()
+
+@app.route("/get_rows")
+def get_rows(chromosome, param_start, param_end):
+	conn = MySQLdb.connect(host= "localhost",
+	                  user="root",
+	                  passwd="master",
+	                  db="genome")
+	x = conn.cursor()
+	strstart = "'%"
+	strend = "%'"
+	keyword = strstart + keyword + strend
+	try:
+		x.execute("SELECT %s FROM human_genome WHERE start >= %d AND end < %d " %(chromosome, param_start, param_end))
+		data = x.fetchall()
 		truedata = json.dumps(data)
 		return truedata
 	except:
